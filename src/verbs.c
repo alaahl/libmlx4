282afde97b776b3f2321bddee22400f1749101fb
Matan Barak <matanb@mellanox.com>
2015-05-03 19:11:53 +0300

    Add support for ibv_create_cq_ex
    Add an extension verb mlx4_create_cq_ex that follows the
    standard extension verb mechanism.
    This function is similar to mlx4_create_cq but supports the
    extension verbs functions and stores the creation flags
    for later use (for example, timestamp flag is used in poll_cq).
    The function fails if the user passes unsupported WC attributes.
    
    issue: 418573
    Change-Id: I6ecfc432db7e22f03f9007f55c15d1841f31cad9
    Signed-off-by: Matan Barak <matanb@mellanox.com>

--- src/verbs.c
+++ src/verbs.c
@@ -287,19 +287,69 @@ int align_queue_size(int req)
 	return nent;
 }
 
-struct ibv_cq *mlx4_create_cq(struct ibv_context *context, int cqe,
-			       struct ibv_comp_channel *channel,
-			       int comp_vector)
+enum cmd_type {
+	MLX4_CMD_TYPE_BASIC,
+	MLX4_CMD_TYPE_EXTENDED
+};
+
+enum {
+	CREATE_CQ_SUPPORTED_COMP_MASK = IBV_CQ_INIT_ATTR_FLAGS
+};
+
+enum {
+	CREATE_CQ_SUPPORTED_FLAGS = IBV_CREATE_CQ_ATTR_COMPLETION_TIMESTAMP
+};
+
+enum {
+	CREATE_CQ_SUPPORTED_WC_FLAGS = IBV_WC_STANDARD_FLAGS
+};
+
+static struct ibv_cq *create_cq(struct ibv_context *context,
+				struct ibv_cq_init_attr_ex *cq_attr,
+				enum cmd_type cmd_type)
 {
-	struct mlx4_create_cq      cmd;
-	struct mlx4_create_cq_resp resp;
-	struct mlx4_cq		  *cq;
-	int			   ret;
-	struct mlx4_context       *mctx = to_mctx(context);
+	struct mlx4_create_cq		cmd;
+	struct mlx4_create_cq_ex	cmd_e;
+	struct mlx4_create_cq_resp	resp;
+	struct mlx4_create_cq_resp_ex	resp_e;
+	struct mlx4_cq			*cq;
+	int				ret;
+	struct mlx4_context		*mctx = to_mctx(context);
+	struct ibv_cq_init_attr_ex	cq_attr_e;
+	int cqe;
 
 	/* Sanity check CQ size before proceeding */
-	if (cqe > 0x3fffff)
+	if (cq_attr->cqe > 0x3fffff)
 		return NULL;
+
+	if (cq_attr->comp_mask & ~CREATE_CQ_SUPPORTED_COMP_MASK) {
+		errno = EINVAL;
+		return NULL;
+	}
+
+	if (cq_attr->comp_mask & IBV_CQ_INIT_ATTR_FLAGS &&
+	    cq_attr->flags & ~CREATE_CQ_SUPPORTED_FLAGS) {
+		errno = EINVAL;
+		return NULL;
+	}
+
+	if (cq_attr->wc_flags & ~CREATE_CQ_SUPPORTED_WC_FLAGS) {
+		errno = ENOTSUP;
+		return NULL;
+	}
+
+	if (!(cq_attr->wc_flags & IBV_WC_EX_WITH_COMPLETION_TIMESTAMP) !=
+	    !(cq_attr->comp_mask & IBV_CQ_INIT_ATTR_FLAGS &&
+	      cq_attr->flags & IBV_CREATE_CQ_ATTR_COMPLETION_TIMESTAMP)) {
+		errno = EINVAL;
+		return NULL;
+	}
+
+	if (cq_attr->wc_flags & IBV_WC_EX_WITH_COMPLETION_TIMESTAMP &&
+	    cq_attr->wc_flags & (IBV_WC_EX_WITH_SL | IBV_WC_EX_WITH_SLID)) {
+		errno = EOPNOTSUPP;
+		return NULL;
+	}
 
 	cq = malloc(sizeof *cq);
 	if (!cq)
@@ -310,9 +360,11 @@ struct ibv_cq *mlx4_create_cq(struct ibv_context *context, int cqe,
 	if (pthread_spin_init(&cq->lock, PTHREAD_PROCESS_PRIVATE))
 		goto err;
 
-	cqe = align_queue_size(cqe + 1);
+	cq_attr_e = *cq_attr;
+	cqe = align_queue_size(cq_attr->cqe + 1);
 
-	if (mlx4_alloc_cq_buf(to_mdev(context->device), &cq->buf, cqe, mctx->cqe_size))
+	if (mlx4_alloc_cq_buf(to_mdev(context->device), &cq->buf, cqe,
+			      mctx->cqe_size))
 		goto err;
 
 	cq->cqe_size = mctx->cqe_size;
@@ -325,15 +377,31 @@ struct ibv_cq *mlx4_create_cq(struct ibv_context *context, int cqe,
 	cq->arm_sn     = 1;
 	*cq->set_ci_db = 0;
 
-	cmd.buf_addr = (uintptr_t) cq->buf.buf;
-	cmd.db_addr  = (uintptr_t) cq->set_ci_db;
+	if (cmd_type == MLX4_CMD_TYPE_BASIC) {
+		cmd.buf_addr = (uintptr_t)cq->buf.buf;
+		cmd.db_addr  = (uintptr_t)cq->set_ci_db;
 
-	ret = ibv_cmd_create_cq(context, cqe - 1, channel, comp_vector,
-				&cq->ibv_cq, &cmd.ibv_cmd, sizeof cmd,
-				&resp.ibv_resp, sizeof resp);
+		ret = ibv_cmd_create_cq(context, cqe - 1,
+					cq_attr_e.channel, cq_attr_e.comp_vector,
+					&cq->ibv_cq, &cmd.ibv_cmd, sizeof(cmd),
+					&resp.ibv_resp, sizeof(resp));
+	} else {
+		cmd_e.buf_addr = (uintptr_t)cq->buf.buf;
+		cmd_e.db_addr  = (uintptr_t)cq->set_ci_db;
+
+		cq_attr_e.cqe = cqe - 1;
+		ret = ibv_cmd_create_cq_ex(context, &cq_attr_e, &cq->ibv_cq,
+					   &cmd_e.ibv_cmd,
+					   sizeof(cmd_e.ibv_cmd), sizeof(cmd_e),
+					   &resp_e.ibv_resp,
+					   sizeof(resp_e.ibv_resp),
+					   sizeof(resp_e));
+	}
+
 	if (ret)
 		goto err_db;
 
+	cq->creation_flags = cmd_e.ibv_cmd.flags;
 	cq->cqn = resp.cqn;
 
 	return &cq->ibv_cq;
@@ -348,6 +416,23 @@ err:
 	free(cq);
 
 	return NULL;
+}
+
+struct ibv_cq *mlx4_create_cq(struct ibv_context *context, int cqe,
+			      struct ibv_comp_channel *channel,
+			      int comp_vector)
+{
+	struct ibv_cq_init_attr_ex attr = {.cqe = cqe, .channel = channel,
+					     .comp_vector = comp_vector,
+					     .wc_flags = IBV_WC_STANDARD_FLAGS};
+
+	return create_cq(context, &attr, MLX4_CMD_TYPE_BASIC);
+}
+
+struct ibv_cq *mlx4_create_cq_ex(struct ibv_context *context,
+				 struct ibv_cq_init_attr_ex *cq_attr)
+{
+	return create_cq(context, cq_attr, MLX4_CMD_TYPE_EXTENDED);
 }
 
 int mlx4_resize_cq(struct ibv_cq *ibcq, int cqe)
